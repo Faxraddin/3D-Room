683b897238d34f1fd9cf2093e9ea2b25e1698103
Faxraddin <reydivonay@gmail.com>
2024-01-06 23:03:28 +0400

    set up + added a 3D cube

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,23 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import React from 'react';
+import './App.css'
+import { Canvas } from '@react-three/fiber';
+
+import Experience from './components/Experience';
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-    
-    </>
+    <Canvas
+      camera={{
+        fov: 64,
+        position: [2.3, 1.5, 2.3],
+      }}
+    >
+      <Experience/>
+    </Canvas>
   )
 }
 
